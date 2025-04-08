51a2a269d156e1864dd09b109c97e544805afcf6
claudiasakkara <claudiasofijohanna@gmail.com>
2025-04-08 11:53:01 +0300

    Päivitetty värit

--- värit.py
+++ värit.py
@@ -9,3 +9,12 @@ YELLOW = '\033[38;2;255;255;135m'
 PINK = '\033[38;2;255;175;255m'
 CYAN = '\033[96m'
 RESET = '\033[0m'
+
+print(f"{BLUE}Kissa koira hevonen karhu hamsteri käärme"{RESET}")
+print(f"{GREEN}Kissa koira hevonen karhu hamsteri käärmef""{RESET}")
+print(f"{LILA}Kissa koira hevonen karhu hamsteri käärmef""{RESET}")
+print(f"{YELLOW}Kissa koira hevonen karhu hamsteri käärmef""{RESET}")
+print(f"{PINK}Kissa koira hevonen karhu hamsteri käärmef""{RESET}")
+print(f"{CYAN}Kissa koira hevonen karhu hamsteri käärmef""{RESET}")
+print(f"{RED}Kissa koira hevonen karhu hamsteri käärmef""{RESET}")
+
