43a020d618f33000ac84a173c0aeddd70c664a24
claudiasakkara <claudiasofijohanna@gmail.com>
2025-04-08 11:22:25 +0300

    Tekstitiedostot

--- Tarot.py
+++ Tarot.py
@@ -1,5 +1,28 @@
+import random
+import textwrap
+
+
+
+def lue_tiedosto(tiedosto, kortin_numero):
+    # Tämä funktio lukee tiedoston ja etsii kortin selityksen kortin numeron perusteella
+    # UTF-8 koodi mahdollistaa erikoismerkkien käsittelyn kuten ääkköset
+    try:
+        with open(tiedosto, "r", encoding="utf-8") as f:
+            sisältö = f.read()
+    except FileNotFoundError:
+        return "Tiedostoa ei löytynyt."
+
+    # Oletetaan, että kortit on erotettu kolmella tyhjällä rivillä = "\n\n\n"
+    kortit = sisältö.split("\n\n\n")
+
+    # For loop käy läpi kaikki kortit ja etsii kortin numeron, välilyönnin ja väliviivan
+    # perusteella oikean kortin selityksen
+    for kortti in kortit:
+        if kortti.startswith(f"{kortin_numero} –"): 
+            return kortti
 
 def vuodenkortti():
+    tiedosto = "vuodenkortit.txt"
 
     paiva = int(input("Anna syntymäpäivä (pp): "))
     kuukausi = int(input("Anna syntymäkuukausi(kk): "))
@@ -10,23 +33,6 @@ def vuodenkortti():
     while luku >= 4: # 4 on lukujen määrä
         luku = sum(int(luku) for luku in str(luku)) # muuttaa yhteenlasketun summan erillisiksi luvuiksi ja laskee yhteen
         return luku
-
-     try:
-        with open(tiedosto, "r", encoding="utf-8") as f:
-            sisältö = f.read()
-    except FileNotFoundError:
-        return "Tiedostoa ei löytynyt."
-
-    # Oletetaan, että kortit on erotettu tyhjällä rivillä = "\n\n"
-    kortit = sisältö.split("\n\n")
-
-    # For loop käy läpi kaikki kortit ja etsii kortin numeron, välilyönnin ja väliviivan
-    # perusteella oikean kortin selityksen
-    for kortti in kortit:
-        if kortti.startswith(f"{luku} –"): 
-            return kortti
-
-    return "Korttia ei löytynyt."
 
 def main():
     tiedosto = "isoarcana.txt"
