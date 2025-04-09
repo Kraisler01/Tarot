b3cadcadfea0e5172c696762787bc90b1ce22e13
Kraisler01 <cristiane.c.harding@gmail.com>
2025-04-09 11:15:44 +0300

    Elämän kortit poikkeukset
    -poikkeukset
    -vuoden jakaminen 2 pareiksi

--- Elämänkortti_testi.py
+++ Elämänkortti_testi.py
@@ -1,16 +1,42 @@
-def elämänkortti(tiedosto, kortin_numero):
+def elämänkortti(tiedosto, kortin_numero, kortti_parit):
 
     paiva = int(input("Anna syntymäpäivä (pp): "))
     kuukausi = int(input("Anna syntymäkuukausi(kk): "))
     vuosi = int(input("Anna syntymävuosi(vvvv): "))
 
 
-
-    if vuoden_parit = [int(vuosi) for vuosi in str(vuosi)] # muuttaa vuoden luvut 2:n pareiksi
+    vuoden_parit = [int(vuosi) for vuosi in str(vuosi)] # muuttaa vuoden luvut 2:n pareiksi
+    
     kortin_numero = paiva + kuukausi + sum(vuoden_parit) # laskee yhteen syntymäpäivän, syntymäkuukauden ja vuoden numerot
-    return kortin_numero # palauttaa kortin numeron
 
-    10 <= kortin_numero <= 99:# jos kortin numero on parillinen
-    kortin_numero = sum(int(kortin_numero) for kortin_numero in str(kortin_numero)) # muuttaa yhteenlasketun summan erillisiksi luvuiksi ja laskee yhteen
-    return kortin_numero 
+    if 10 <= kortin_numero <= 99: # tarkistaa, että kortin numero on kahden numeron luku
+        kortin_numero = sum(int(kortin_numero) for kortin_numero in str(kortin_numero)) # muuttaa yhteenlasketun summan erillisiksi luvuiksi ja laskee yhteen
+        
+
+#poikkeukset, jotka eivät mene yhteen kortti parien kanssa
+#eli lasketaan eka normi kortit ja jos luku on 2 lukuinen niin ensimmäinen summa on 2 lukuinen 
+#sen summa on eka kortti ja korttiparit ovat poikkeukset tässä alapuolella
+
+
+    elif kortin_numero == 8:
+        Kortti_parit = 17
+
+    elif kortin_numero == 19:
+        kortti_parit = #maagikko 3 korttia, kortti 1,10,19
+    elif kortin_numero == 10:
+        kortti_parit = 1
+
+    elif kortin_numero == 11:
+        kortti_parit = 2
+    elif kortin_numero == 20:
+        kortti_parit = 2
+
+    elif kortin_numero == 12:
+        kortti_parit = 3
+    elif kortin_numero == 21:
+        kortti_parit = 3
+
+
+
+
 
