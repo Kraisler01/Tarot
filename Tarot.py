8fc934b309ce1c99499dee9ba099b230c8acb306
claudiasakkara <claudiasofijohanna@gmail.com>
2025-04-16 14:37:22 +0300

    Update Tarot.py

--- Tarot.py
+++ Tarot.py
@@ -225,35 +225,55 @@ def kortin_selitys():
 
 
 def main():
-    print(f"{LILA}""\nTervetuloa korttien viisauden äärelle - tässä tilassa universumi puhuu!"f"{RESET}")
+
+    print(f"{YELLOW}""\nTervetuloa korttien viisauden äärelle - tässä tilassa universumi puhuu!"f"{RESET}")
+    print(f'''
+    {LILA}Ohjeita käyttäjälle:{RESET}
+            
+        {ORANGE}1 – Vuoden kortti{RESET}
+    {LILA}Nosta esiin vuoden matkan ydinenergia. Mitä oppia, valoa tai haastetta tämä vuosi tuo sinulle?{RESET}
+
+        {PINK}2 – Elämän kortit{RESET}
+    {LILA}Nämä kortit kulkevat kanssasi koko elämäsi – kuin sielun kompassi.{RESET}
+
+        {GREEN}3 – Kysy korteilta{RESET}
+    {LILA}Onko mielesi mutkalla? Nosta ohjaava kortti ja anna sen viestin valaista tiesi.{RESET}
+
+        {BLUE}4 – Kortin selitys{RESET}
+    {LILA}Haluatko ymmärtää tiettyä korttia syvemmin? Syötä kortin numero ja näät selityksen.{RESET}
+        ''')
+    
+while True:
     time.sleep(1)
     nayta_valikko()
+    try:
+        valinta = input(f"{LILA}""Valintasi (0/1/2/3/4): "f"{RESET}")
 
-    valinta = input(f"{LILA}""Valintasi (0/1/2/3/4): "f"{RESET}")
+        if valinta == "1":
+            vuodenkortti()
+        elif valinta == "2":
+            elaman_kortti()
+        elif valinta == "3":
+            satunnainen_kortti()
+        elif valinta == "4":
+            kortin_selitys()
+        elif valinta == "0":
+            print(f"{YELLOW}""\n*✧･ﾟ: *✧･ﾟKiitos ja näkemiin! ☾ ⋆*･ﾟ:⋆*･ﾟ\n"f"{RESET}")
+            break
+        else:
+            print(f"{LILA}""\nJos haluat, voimme kokeilla uudelleen. Valitse intuitiosi avulla"f"{RESET}")
 
-    if valinta == "1":
-        vuodenkortti()
-    elif valinta == "2":
-        elaman_kortti()
-    elif valinta == "3":
-        satunnainen_kortti()
-    elif valinta == "4":
-        kortin_selitys()
-    elif valinta == "0":
-        print(f"{YELLOW}""\n*✧･ﾟ: *✧･ﾟKiitos ja näkemiin! ☾ ⋆*･ﾟ:⋆*･ﾟ\n"f"{RESET}")
-        return
-    else:
-        print(f"{LILA}""\nJos haluat, voimme kokeilla uudelleen. Valitse intuitiosi avulla"f"{RESET}")
+        print(f"{LILA}""\nKaipaavatko sydämesi ja mielesi vielä lisää opastusta korttien kautta?\n"f"{RESET}")
+        another_one = input(f"{LILA}""Valinta on sinun (1 Kyllä /2 Ei): "f"{RESET}")
+        if another_one == "1":
+            continue
+        if another_one == "2":
+            print(f"{LILA}""\nIstuntomme päättyy, mutta korttien taika jää kanssasi. Pidä huolta itsestäsi!\n"f"{RESET}")
+            break
+        else:
+            print(f"{LILA}""\nValinta ei ole oikein, mutta voit aina palata korttien pariin. Kiitos ja näkemiin!\n"f"{RESET}")
+            break
 
-    print(f"{LILA}""\nKaipaavatko sydämesi ja mielesi vielä lisää opastusta korttien kautta?\n"f"{RESET}")
-    another_one = input(f"{LILA}""Valinta on sinun (1 Kyllä /2 Ei): "f"{RESET}")
-    if another_one == "1":
-        main()
-    elif another_one == "2":
-        print(f"{LILA}""\nIstuntomme päättyy, mutta korttien taika jää kanssasi. Pidä huolta itsestäsi!\n"f"{RESET}")
-    else:
-        print(f"{LILA}""\nValinta ei ole oikein, mutta voit aina palata korttien pariin. Kiitos ja näkemiin!\n"f"{RESET}")
+    except Exception as e: # Käsittelee arvaamattomat yleisimmät virheet, tallentaa ne muuttujaan ja tulostaa tiedot käyttäjälle
+        print(f"Tapahtui virhe: {e}.")
 main()
-
-
-# elämän korttien tulostetta vois muokata jotenkin selkeemmäks niin että lahjat ja haasteet olis omilla riveillä
