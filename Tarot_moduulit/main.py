5979a0d5ec2ca25061992d083647b44c8b484f16
elssu <elsa.viheroja@gmail.com>
2025-04-16 12:39:56 +0300

    poikpoik
    poikkeustenkäsittelyä

--- Tarot_moduulit/main.py
+++ Tarot_moduulit/main.py
@@ -23,32 +23,35 @@ def main():
     while True:
         time.sleep(1)
         nayta_valikko()
+        try:
+            valinta = input(f"{LILA}""Valintasi (0/1/2/3/4): "f"{RESET}")
 
-        valinta = input(f"{LILA}""Valintasi (0/1/2/3/4): "f"{RESET}")
+            if valinta == "1":
+                laskettavat_kortit.vuodenkortti()
+            elif valinta == "2":
+                laskettavat_kortit.elaman_kortti()
+            elif valinta == "3":
+                nosta_kortti.satunnainen_kortti()
+            elif valinta == "4":
+                nosta_kortti.kortin_selitys()
+            elif valinta == "0":
+                print(f"{YELLOW}""\n*✧･ﾟ: *✧･ﾟKiitos ja näkemiin! ☾ ⋆*･ﾟ:⋆*･ﾟ\n"f"{RESET}")
+                break
+            else:
+                print(f"{LILA}""\nJos haluat, voimme kokeilla uudelleen. Valitse intuitiosi avulla"f"{RESET}")
 
-        if valinta == "1":
-            laskettavat_kortit.vuodenkortti()
-        elif valinta == "2":
-            laskettavat_kortit.elaman_kortti()
-        elif valinta == "3":
-            nosta_kortti.satunnainen_kortti()
-        elif valinta == "4":
-            nosta_kortti.kortin_selitys()
-        elif valinta == "0":
-            print(f"{YELLOW}""\n*✧･ﾟ: *✧･ﾟKiitos ja näkemiin! ☾ ⋆*･ﾟ:⋆*･ﾟ\n"f"{RESET}")
-            break
-        else:
-            print(f"{LILA}""\nJos haluat, voimme kokeilla uudelleen. Valitse intuitiosi avulla"f"{RESET}")
+            print(f"{LILA}""\nKaipaavatko sydämesi ja mielesi vielä lisää opastusta korttien kautta?\n"f"{RESET}")
+            another_one = input(f"{LILA}""Valinta on sinun (1 Kyllä /2 Ei): "f"{RESET}")
 
-        print(f"{LILA}""\nKaipaavatko sydämesi ja mielesi vielä lisää opastusta korttien kautta?\n"f"{RESET}")
-        another_one = input(f"{LILA}""Valinta on sinun (1 Kyllä /2 Ei): "f"{RESET}")
+            if another_one == "1":
+                continue
+            if another_one == "2":
+                print(f"{LILA}""\nIstuntomme päättyy, mutta korttien taika jää kanssasi. Pidä huolta itsestäsi!\n"f"{RESET}")
+                break
+            else:
+                print(f"{LILA}""\nValinta ei ole oikein, mutta voit aina palata korttien pariin. Kiitos ja näkemiin!\n"f"{RESET}")
+                break
 
-        if another_one == "1":
-            continue
-        if another_one == "2":
-            print(f"{LILA}""\nIstuntomme päättyy, mutta korttien taika jää kanssasi. Pidä huolta itsestäsi!\n"f"{RESET}")
-            break
-        else:
-            print(f"{LILA}""\nValinta ei ole oikein, mutta voit aina palata korttien pariin. Kiitos ja näkemiin!\n"f"{RESET}")
-            break
+        except Exception as e: # Käsittelee arvaamattomat yleisimmät virheet, tallentaa ne muuttujaan ja tulostaa tiedot käyttäjälle
+            print(f"Tapahtui virhe: {e}.")
 main()
