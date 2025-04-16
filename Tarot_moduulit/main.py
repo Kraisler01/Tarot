d27444dfedba07a9e1b11668dabd47a4837dc67f
elssu <elsa.viheroja@gmail.com>
2025-04-16 12:02:18 +0300

    while
    while-loop mainiin

--- Tarot_moduulit/main.py
+++ Tarot_moduulit/main.py
@@ -16,34 +16,39 @@ def nayta_valikko():
         {DEEPMAGENTA}0 Lopeta ohjelma{RESET}
         ''')
 
-
 def main():
+
     print(f"{LILA}""\nTervetuloa korttien viisauden äärelle - tässä tilassa universumi puhuu!"f"{RESET}")
-    time.sleep(1)
-    nayta_valikko()
 
-    valinta = input(f"{LILA}""Valintasi (0/1/2/3/4): "f"{RESET}")
+    while True:
+        time.sleep(1)
+        nayta_valikko()
 
-    if valinta == "1":
-        laskettavat_kortit.vuodenkortti()
-    elif valinta == "2":
-        laskettavat_kortit.elaman_kortti()
-    elif valinta == "3":
-        nosta_kortti.satunnainen_kortti()
-    elif valinta == "4":
-        nosta_kortti.kortin_selitys()
-    elif valinta == "0":
-        print(f"{YELLOW}""\n*✧･ﾟ: *✧･ﾟKiitos ja näkemiin! ☾ ⋆*･ﾟ:⋆*･ﾟ\n"f"{RESET}")
-        return
-    else:
-        print(f"{LILA}""\nJos haluat, voimme kokeilla uudelleen. Valitse intuitiosi avulla"f"{RESET}")
+        valinta = input(f"{LILA}""Valintasi (0/1/2/3/4): "f"{RESET}")
 
-    print(f"{LILA}""\nKaipaavatko sydämesi ja mielesi vielä lisää opastusta korttien kautta?\n"f"{RESET}")
-    another_one = input(f"{LILA}""Valinta on sinun (1 Kyllä /2 Ei): "f"{RESET}")
-    if another_one == "1":
-        main()
-    elif another_one == "2":
-        print(f"{LILA}""\nIstuntomme päättyy, mutta korttien taika jää kanssasi. Pidä huolta itsestäsi!\n"f"{RESET}")
-    else:
-        print(f"{LILA}""\nValinta ei ole oikein, mutta voit aina palata korttien pariin. Kiitos ja näkemiin!\n"f"{RESET}")
+        if valinta == "1":
+            laskettavat_kortit.vuodenkortti()
+        elif valinta == "2":
+            laskettavat_kortit.elaman_kortti()
+        elif valinta == "3":
+            nosta_kortti.satunnainen_kortti()
+        elif valinta == "4":
+            nosta_kortti.kortin_selitys()
+        elif valinta == "0":
+            print(f"{YELLOW}""\n*✧･ﾟ: *✧･ﾟKiitos ja näkemiin! ☾ ⋆*･ﾟ:⋆*･ﾟ\n"f"{RESET}")
+            break
+        else:
+            print(f"{LILA}""\nJos haluat, voimme kokeilla uudelleen. Valitse intuitiosi avulla"f"{RESET}")
+
+        print(f"{LILA}""\nKaipaavatko sydämesi ja mielesi vielä lisää opastusta korttien kautta?\n"f"{RESET}")
+        another_one = input(f"{LILA}""Valinta on sinun (1 Kyllä /2 Ei): "f"{RESET}")
+
+        if another_one == "1":
+            continue
+        if another_one == "2":
+            print(f"{LILA}""\nIstuntomme päättyy, mutta korttien taika jää kanssasi. Pidä huolta itsestäsi!\n"f"{RESET}")
+            break
+        else:
+            print(f"{LILA}""\nValinta ei ole oikein, mutta voit aina palata korttien pariin. Kiitos ja näkemiin!\n"f"{RESET}")
+            break
 main()
