59d6e386444c0da43ca0d9ec65b1cce68f2dc478
Kraisler01 <cristiane.c.harding@gmail.com>
2025-04-16 14:22:42 +0300

    Lista tehty

--- Tarot_moduulit/nosta_kortti.py
+++ Tarot_moduulit/nosta_kortti.py
@@ -8,6 +8,12 @@ def kortin_selitys():
     tiedosto = "isoarcana.txt"
     text = '☾ ⋆*･ﾟ:⋆*･ﾟ'
 
+    kortit_lista = ['0. Narri,','1. Maagikko', '2. Ylipapitar', '3. Hallitsijatar', '4. Hallitsija', '5. Ylipappi',
+                '6. Rakastavaiset', '7. Vaunut', '8. Voima', '9. Erakko', '10. Kohtalonpyörä', '11. Oikeus', '12. Hirtetty',
+                '13. Kuolema', '14. Kohtuus', '15. Paholainen', '16. Torni', '17. Tähti', '18. Kuu', '19. Aurinko', '20. Tuomio', '21. Maailma']
+
+    for kortti in kortit_lista:
+        print(kortti)
     #Tehdään looppi, joka pyytää käyttäjältä kortin numeroa
     #ja tarkistaa, että se on oikea
     while True: 
